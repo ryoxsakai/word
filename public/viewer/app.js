0b49eb3edb1ac76e48834b26e72a805a5ea5178a
Ryo SAKAI <ryoxsakai@gmail.com>
2026-09-06 22:53:29 +0900

    Place first chapter door before word list

--- public/viewer/app.js
+++ public/viewer/app.js
@@ -1778,7 +1778,11 @@ function preparePrintHierarchy() {
     chapterDivider.style.setProperty("--chapter-color", sectionStyle.getPropertyValue("--chapter-color"));
     chapterDivider.style.setProperty("--hierarchy-copy-column", "17mm");
     chapterDivider.classList.add("print-chapter-door");
+    sectionGroup.before(chapterDivider);
+    sectionGroup.classList.remove("has-chapter-divider");
   }
+  const firstChapterDoor = el.wordList.querySelector(":scope > .print-chapter-door");
+  if (firstChapterDoor) el.wordList.before(firstChapterDoor);
 }
 
 function printSectionKeys() {
